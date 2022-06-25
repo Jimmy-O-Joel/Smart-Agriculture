54ff4ac89433a945ba75342018802e2f752f03ba
Jimmy Joel <jimmyjoel177@gmail.com>
2022-06-25 10:42:31 +0300

    revision

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose")
 const app = express()
 
 app.set("view engine", "ejs")
+
+//body parser middleware
+app.use(bodyParser.json)
 app.use(bodyParser.urlencoded({ extended: true }))
+
+//set public folder
 app.use(express.static("public"))
 
 mongoose.connect("mongodb+srv://admin-jimmy:SAWA12120@cluster0.hrd38.mongodb.net/SmartAgriculture")
@@ -20,11 +25,10 @@ const moisture = new Moisture({
     value: 20
 })
 
-moisture.save()
+//moisture.save()
 
 
 app.get("/", function (req, res) {
-
     Moisture.find(function (err, moistures) {
         if (err) {
             console.log(err)
@@ -45,6 +49,11 @@ app.get("/", function (req, res) {
 
 })
 
-app.listen(3000, function () {
-    console.log("Server started successfully on port 3000")
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log(`Server started successfully on port ${port}`)
 })
