508591465a1953817691e073a39425fa1c1df233
Jimmy Joel <jimmyjoel177@gmail.com>
2022-06-25 14:01:44 +0300

    make app more responsive

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const Pusher = require("pusher");
 
 const app = express()
 
 app.set("view engine", "ejs")
 
-//body parser middleware
-app.use(bodyParser.json)
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //set public folder
 app.use(express.static("public"))
 
 mongoose.connect("mongodb+srv://admin-jimmy:SAWA12120@cluster0.hrd38.mongodb.net/SmartAgriculture")
+
+const pusher = new Pusher({
+  appId: "1428509",
+  key: "4d4f0d0cf3b7fc4b6e60",
+  secret: "a7dbb63c021e9d95f5b4",
+  cluster: "mt1",
+  useTLS: true
+});
+
+const db = mongoose.connection;
+db.once("open", ()=>{
+    const moistureCollection = db.collection("moistures")
+    const changeStream = moistureCollection.watch()
+    changeStream.on("change", (change)=>{
+        if (change.operationType === "insert") {
+            const moisture = change.fullDocument
+            pusher.trigger("newValue", "newValue", moisture.value)
+        }
+
+    })
+})
 
 const moistureSchema = new mongoose.Schema({
     value: Number
@@ -28,7 +48,7 @@ const moisture = new Moisture({
 //moisture.save()
 
 app.get("/", function (req, res) {
-    Moisture.find(function (err, moistures) {
+    Moisture.find((err, moistures)=>{
         if (err) {
             console.log(err)
         } else {
@@ -50,9 +70,9 @@ app.get("/", function (req, res) {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 3000;
+    port = 3000
 }
 
-app.listen(port, function () {
+app.listen(port, ()=>{
     console.log(`Server started successfully on port ${port}`)
 })
